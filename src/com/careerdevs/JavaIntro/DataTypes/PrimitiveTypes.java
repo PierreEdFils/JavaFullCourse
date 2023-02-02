8422601f3265710825bc9f20251e53247db90273
Pierre Fils <epierrefils@gmail.com>
2023-02-01 21:48:51 -0500

    Recap of Java

---
+++ src/com/careerdevs/JavaIntro/DataTypes/PrimitiveTypes.java
@@ -0,0 +1,10 @@
+package com.careerdevs.JavaIntro.DataTypes;
+
+public class PrimitiveTypes {
+    byte age =30;
+    long viewCount =3_123_456_789L;
+    float price= 10.99F;
+    char letter ='A';
+    boolean isEligible = false;
+
+}
---
+++ src/com/careerdevs/JavaIntro/DataTypes/Variables.java
@@ -0,0 +1,12 @@
+package com.careerdevs.JavaIntro.DataTypes;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myAge = 30;
+        int herAge= myAge;
+
+        System.out.println(herAge);
+    }
+
+
+}
