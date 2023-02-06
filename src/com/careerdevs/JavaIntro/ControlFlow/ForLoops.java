a351e1747624cc90c1a94a93915429e9bf3d7df4
Pierre Fils <epierrefils@gmail.com>
2023-02-05 23:32:16 -0500

    Conrol Flow to Whike Loops

---
+++ src/com/careerdevs/JavaIntro/ControlFlow/ForLoops.java
@@ -0,0 +1,8 @@
+package com.careerdevs.JavaIntro.ControlFlow;
+
+public class ForLoops {
+    public static void main(String[] args) {
+        for (int i = 5; i > 0; i--)
+            System.out.println("Hello World" +  i );
+    }
+}
