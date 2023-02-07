a91f190e309954fc8e83eb33a715a0e5cee8afd5
Pierre Fils <epierrefils@gmail.com>
2023-02-06 23:53:23 -0500

    Clean coding Logical Operators

---
+++ src/com/careerdevs/JavaIntro/CleanCoding/LogicalOperators.java
@@ -0,0 +1,14 @@
+package com.careerdevs.JavaIntro.CleanCoding;
+
+public class LogicalOperators {
+    public static void main(String[] args) {
+                int  temperature =12;
+        boolean isWarm = temperature > 20 && temperature<30;
+        System.out.println(isWarm);
+
+        boolean hasHighIncome = true;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolean isEligible  = (hasHighIncome ||  hasGoodCredit) && !hasCriminalRecord;
+    }
+}
